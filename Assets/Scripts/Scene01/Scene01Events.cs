ab020ec3c294fca30f3bf9601df10cab25956a80
Lombardo Neuman <100061697@csisd.org>
2026-09-17 13:34:17 -0500

    Started on first scene
    Have to fix the next button, doesn't go to next dialogue.

--- Assets/Scripts/Scene01/Scene01Events.cs
+++ Assets/Scripts/Scene01/Scene01Events.cs
@@ -5,17 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Scene01Events : MonoBehaviour
 {
-    [Header("Character Expressions")]
-    public GameObject charKodaNeutral;
-    public GameObject charKodaSurprised;
-    public GameObject charKodaSmirk;
-    public GameObject charKodaSmile;
-    public GameObject charKodaEmbarrassed;
-    
     public GameObject fadeScreenIn;
     public GameObject textBox;
+
     [SerializeField] AudioSource girlSigh;
-    [SerializeField] AudioSource girlGasp;
     [SerializeField] string textToSpeak;
     [SerializeField] int currentTextLength;
     [SerializeField] int textLength;
@@ -25,10 +18,11 @@ public class Scene01Events : MonoBehaviour
     [SerializeField] GameObject charName;
     [SerializeField] GameObject fadeOut;
 
-    // index within the intro dialogue sequence
+    // Index within the intro dialogue sequence
     int introIndex = 0;
-    // index within the Koda dialogue sequence (Event 1)
-    int kodaIndex = 0;
+
+    // Index within the dialogue sequence
+    int dialogueIndex = 0;
 
     void Update()
     {
@@ -40,88 +34,82 @@ public class Scene01Events : MonoBehaviour
         StartCoroutine(EventStarter());
     }
 
-    void HideAllExpressions()
-    {
-        charKodaNeutral.SetActive(false);
-        charKodaSurprised.SetActive(false);
-        charKodaSmirk.SetActive(false);
-        charKodaSmile.SetActive(false);
-        charKodaEmbarrassed.SetActive(false);
-    }
-
-    void ShowExpression(string expression)
-    {
-        HideAllExpressions();
-        switch (expression)
-        {
-            case "neutral":
-                charKodaNeutral.SetActive(true);
-                break;
-            case "surprised":
-                charKodaSurprised.SetActive(true);
-                break;
-            case "smirk":
-                charKodaSmirk.SetActive(true);
-                break;
-            case "smile":
-                charKodaSmile.SetActive(true);
-                break;
-            case "embarrassed":
-                charKodaEmbarrassed.SetActive(true);
-                break;
-        }
-    }
-
     IEnumerator EventStarter()
     {
-        // initial fade & character appear (Koda neutral initially)
+        // Initial fade
         yield return new WaitForSeconds(2f);
+
         fadeScreenIn.SetActive(false);
-        ShowExpression("neutral");
+
         yield return new WaitForSeconds(2f);
 
         mainTextObject.SetActive(true);
         textBox.SetActive(true);
-        charName.GetComponent<TMPro.TMP_Text>().text = "Koda";
 
-        // start first intro line
+        // Start first intro line
         introIndex = 0;
-        yield return StartCoroutine(PlayIntroLine(introIndex));
+        eventPos = 0;
 
-        eventPos = 0; // 0 = still in intro sequence
+        yield return StartCoroutine(PlayIntroLine(introIndex));
     }
 
     IEnumerator PlayIntroLine(int index)
     {
         nextButton.SetActive(false);
 
-        // Set expression based on line
         switch (index)
         {
-            case 0: // "Oh! My bad..."
-                ShowExpression("surprised");
-                textToSpeak = "Oh! My bad, this always happens on this bump.";
+            case 0:
+                textToSpeak = "Bay could only hear the sounds of people around him, being concious enough to know that he was in a public place, but not concious enough to know where he was or what was going on.";
                 break;
-            case 1: // laugh
-                ShowExpression("smile");
-                textToSpeak = "[He laughs, brushing his hair back]";
+
+            case 1:
+                textToSpeak = "Who is this?";
                 break;
-            case 2: // excited about meeting
-                ShowExpression("smile");
-                textToSpeak = "Wait… You're the new transfer student, right? No way! I actually got to meet you first.";
+
+            case 2:
+                textToSpeak = "Where’d he come from?";
                 break;
-            case 3: // confident
-                ShowExpression("smirk");
-                textToSpeak = "[He grins] Guess that means I've got a head start on being your first friend!";
+
+            case 3:
+                textToSpeak = "From up there, you silly!";
                 break;
+
+            case 4:
+                textToSpeak = "Look at his hair, it’s blonde!";
+                break;
+
+            case 5:
+                textToSpeak = "Is he okay? Is he dead?!";
+                break;
+
+            case 6:
+                textToSpeak = "...";
+                break;
+
+            case 7:
+                textToSpeak = "They’re definitely getting him…";
+                break;
+
+            default:
+                yield break;
         }
 
-        charName.GetComponent<TMPro.TMP_Text>().text = "Koda";
+        // Clear character name during intro
+        charName.GetComponent<TMPro.TMP_Text>().text = "";
 
+        // Set dialogue text
         textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+
         currentTextLength = textToSpeak.Length;
+
+        // Reset text counter if TextCreator allows it
+        TextCreator.charCount = 0;
+
+        // Start typewriter effect
         TextCreator.runTextPrint = true;
 
+        // Play sigh on first intro line
         if (index == 0 && girlSigh != null)
         {
             girlSigh.Play();
@@ -129,109 +117,145 @@ public class Scene01Events : MonoBehaviour
 
         yield return new WaitForSeconds(0.05f);
         yield return new WaitForSeconds(1f);
+
+        // Wait until all characters have printed
         yield return new WaitUntil(() => textLength >= currentTextLength);
+
         yield return new WaitForSeconds(0.5f);
+
+        // Allow player to continue
         nextButton.SetActive(true);
     }
 
-    IEnumerator PlayKodaLine(int index)
+    IEnumerator PlayDialogueLine(int index)
     {
         nextButton.SetActive(false);
 
-        // Set expression and name based on line
         bool isNarration = (index == 0 || index == 6 || index == 7);
-        charName.GetComponent<TMPro.TMP_Text>().text = isNarration ? "You" : "Koda";
+
+        charName.GetComponent<TMPro.TMP_Text>().text =
+            isNarration ? "You" : "";
 
         switch (index)
         {
             case 0:
-                ShowExpression("surprised");
                 textToSpeak = "[You feel a surge of energy coursing through you. He facepalms]";
                 break;
+
             case 1:
-                ShowExpression("smile");
-                textToSpeak = "Oh, I almost forgot to introduce myself. My name is Koda Sable, but you can just call me Koda.";
+                textToSpeak = "Oh, I almost forgot to introduce myself. My name is Sable, but you can just call me Sable.";
                 break;
+
             case 2:
-                ShowExpression("neutral");
                 textToSpeak = "Anyways, we're close to school. Bummer…";
                 break;
+
             case 3:
-                ShowExpression("smile");
                 textToSpeak = "I hope we have classes together, we can sneak notes and stuff!";
                 break;
+
             case 4:
-                ShowExpression("embarrassed"); // Perfect for "lovers" slip-up!
                 textToSpeak = "Wait no, that's what lovers do.";
                 break;
+
             case 5:
-                ShowExpression("smile");
                 textToSpeak = "You know what, let's hang out after school!";
                 break;
+
             case 6:
-                ShowExpression("smile"); // Neutral for player narration
                 textToSpeak = "[You felt determined to make the most out of this situation. You agreed to hanging out with him after school.]";
                 break;
+
             case 7:
-                ShowExpression("smile");
                 textToSpeak = "[What could be the worst that could happen?]";
                 break;
+
+            default:
+                yield break;
         }
 
+        // Set dialogue text
         textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+
         currentTextLength = textToSpeak.Length;
+
+        // Reset text counter
+        TextCreator.charCount = 0;
+
+        // Start typewriter effect
         TextCreator.runTextPrint = true;
 
         yield return new WaitForSeconds(0.05f);
         yield return new WaitForSeconds(1f);
+
+        // Wait until all characters have printed
         yield return new WaitUntil(() => textLength >= currentTextLength);
+
         yield return new WaitForSeconds(0.5f);
+
+        // Allow player to continue
         nextButton.SetActive(true);
     }
 
     IEnumerator EventFour()
     {
         nextButton.SetActive(false);
-        ShowExpression("smile"); // Final smile before fade
+
         textBox.SetActive(true);
+
         fadeOut.SetActive(true);
+
         yield return new WaitForSeconds(4f);
+
         SceneManager.LoadScene(3);
     }
 
     public void NextButton()
     {
+        // INTRO SEQUENCE
         if (eventPos == 0)
         {
             introIndex++;
-            if (introIndex <= 3)
+
+            // There are 8 intro lines: 0 through 7
+            if (introIndex <= 7)
             {
                 StartCoroutine(PlayIntroLine(introIndex));
             }
             else
             {
+                // Intro finished
                 eventPos = 1;
-                kodaIndex = 0;
-                StartCoroutine(PlayKodaLine(kodaIndex));
+                dialogueIndex = 0;
+
+                StartCoroutine(PlayDialogueLine(dialogueIndex));
             }
+
             return;
         }
 
+        // DIALOGUE SEQUENCE
         if (eventPos == 1)
         {
-            kodaIndex++;
-            if (kodaIndex <= 7)
+            dialogueIndex++;
+
+            // There are 8 dialogue lines: 0 through 7
+            if (dialogueIndex <= 7)
             {
-                StartCoroutine(PlayKodaLine(kodaIndex));
+                StartCoroutine(PlayDialogueLine(dialogueIndex));
             }
             else
             {
+                // Dialogue finished
                 eventPos = 4;
+
                 StartCoroutine(EventFour());
             }
+
             return;
         }
 
+        // END EVENT
         if (eventPos == 4)
         {
             StartCoroutine(EventFour());
